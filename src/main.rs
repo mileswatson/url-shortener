fb1d1c6ccc9d705ba1c57277d39ecf98a20faa38
Miles Watson <contact@mileswatson.net>
2021-09-08 13:47:54 +0100

    Added shorten and redirect endpoints.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 use dashmap::DashMap;
+use rand::{thread_rng, Rng};
+use rocket::{
+    response::{
+        status::{BadRequest, NotFound},
+        Redirect,
+    },
+    State,
+};
 
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[post("/api/shorten?<url>")]
+fn shorten(url: String, state: &State<DashMap<u32, String>>) -> Result<String, BadRequest<&str>> {
+    if url.is_empty() {
+        Err(BadRequest(Some("URL is empty!")))
+    } else {
+        let key: u32 = thread_rng().gen();
+        state.insert(key, url);
+        Ok(key.to_string())
+    }
+}
+
+#[get("/<key>")]
+fn redirect(key: u32, state: &State<DashMap<u32, String>>) -> Result<Redirect, NotFound<&str>> {
+    state
+        .get(&key)
+        .map(|url| Redirect::to(url.clone()))
+        .ok_or(NotFound("Invalid or expired link!"))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(DashMap::<String, String>::new())
-        .mount("/", routes![index])
+        .manage(DashMap::<u32, String>::new())
+        .mount("/", routes![shorten, redirect])
 }
