ec56d50d38b6a801669995d12ff7052f2127255a
Miles Watson <contact@mileswatson.net>
2021-09-20 16:39:22 +0100

    Added functionality to Svelte site.

---
+++ svelte/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<div id="svelte" class="container is-fluid my-5">
+    <nav class="navbar is-dark" role="navigation">
+        <div class="navbar-brand">
+            <div class="navbar-item ml-5 is-dark">
+                <img src="/favicon.png" width="32" height="32" alt="logo" />
+            </div>
+            <h1 class="title is-2 navbar-item">URL Shortener</h1>
+        </div>
+    </nav>
+
+    <slot />
+</div>
--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import superagent from "superagent";
+
+    let url = "";
+    let request = null;
+
+    function click() {
+        request = superagent.post(`/api/shorten?url=${url}`);
+    }
+
+    function getUrl(key) {
+        return `http://${window.location.host}/${key}`;
+    }
+</script>
+
+<div class="box">
+    {#if request == null}
+        <div class="field has-addons">
+            <div class="control">
+                <input
+                    class="input"
+                    type="text"
+                    bind:value={url}
+                    placeholder="URL"
+                />
+            </div>
+            <div class="control">
+                <button class="button is-info" on:click={click}>Shorten</button>
+            </div>
+        </div>
+    {:else}
+        {#await request}
+            <p>Loading...</p>
+        {:then response}
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">Done!</p>
+                </header>
+                <div class="card-content">
+                    <a
+                        class="content"
+                        href={getUrl(response.text)}
+                        target="_blank">{getUrl(response.text)}</a
+                    >
+                </div>
+                <footer class="card-footer">
+                    <button
+                        class="card-footer-item button"
+                        on:click={() => (request = null)}>Back</button
+                    >
+                    <button
+                        class="card-footer-item button is-info"
+                        on:click={() =>
+                            navigator.clipboard.writeText(
+                                getUrl(response.text)
+                            )}>Copy</button
+                    >
+                </footer>
+            </div>
+        {:catch}
+            <p>Something went wrong!</p>
+        {/await}
+    {/if}
+</div>
